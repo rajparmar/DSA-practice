25602fbdad56d044dd29650330b564e14f7b6151
rajparmar <rajparmarjb43@gmail.com>
2026-02-24 20:21:28 +0530

    2nd half

--- STL.cpp
+++ STL.cpp
@@ -6,7 +6,7 @@ int main (){
     // auto  last =v.back();
     // cout << last ;
      vector<int> arr;
- int x;
+ int x,y,z,a,b,c,d,e;
  // here you can see what happens when cin >> is used as a condition it yes true until null is passed
     while (cin >> x ){
      arr.push_back(x);
@@ -15,7 +15,27 @@ int main (){
     for (auto it : arr){
         cout << it << endl;
     }
+     cin>> x;
+     cin>> y;
+     cin>> z;
+     cin.clear();
+     cin>> a;
+     cin>> b;
+     cin>> c;
+     cin>> d;
+     cin>> e ;
 
+     cout << endl << endl << x << endl;
+     cout << y << endl;
+     cout << z << endl;
+     cout << a << endl;
+     cout << b << endl;
+     cout << c << endl;
+     cout << d << endl;
+     cout << e << endl;
+     cout << x << endl;
+  
+   
 
 
 }
