22b96bde0eda98f4142b9c80802eef4de8c6d5e2
rajparmar <rajparmarjb43@gmail.com>
2026-07-29 20:04:53 +0530

    subsum

---
+++ subseqsum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+void subsum(int i, vector<int > v, int sum){
+       
+}
+
+int main (){
+     int n ,inp ;
+    cin  >> n ;
+    vector <int> a;
+    for (int i =0 ; i< n ; i++){
+        cin >> inp;
+        a.push_back(inp);
+    }
+    
+    int sum; 
+    cin >> sum; 
+
+    subsum(0,a,sum);
+
+ 
+
+}
