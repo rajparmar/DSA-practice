0cc700d6a0d74ad8fdf750512bcd5f56c70ece86
rajparmar <rajparmarjb43@gmail.com>
2026-07-29 19:02:29 +0530

    subsequence

---
+++ subsequence.cpp
@@ -0,0 +1,79 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+//  void subsequence( int i , vector <int> a , vector <int> b){
+     
+//     if (i>=a.size()){
+//         cout << "{";
+//         for (auto x : b){
+//             cout << x << " ";
+            
+//         } 
+//         cout << "}";
+//     cout << endl;
+//     return ;
+//   }
+     
+//     b.push_back(a[i]) ;
+//     subsequence(i+1,a,b);
+    
+//     b.pop_back();
+//     subsequence(i+1,a,b);
+    
+//      return ;
+//  }
+
+// int main (){
+//     int n ,inp ;
+//     cin >> n ;
+//     vector <int> arr;
+//     vector <int> sub = {};
+//     for (int i =0;i<n;i++){
+//         cin>>inp;
+//         arr.push_back(inp);
+//     }
+    
+//     subsequence (0, arr, sub);
+   
+//     return 0 ;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ void subsequence( int i , vector <int> &a , vector <int> &b){
+     
+    if (i>=a.size()){
+        cout << "{";
+        for (auto x : b){
+            cout << x ;
+            
+        } 
+        cout << "}";
+    cout << endl;
+    return ;
+  }
+     
+    b.push_back(a[i]) ;
+    subsequence(i+1,a,b);
+    
+    b.pop_back();
+    subsequence(i+1,a,b);
+    
+     return ;
+ }
+
+int main (){
+    int n ,inp ;
+    cin >> n ;
+    vector <int> arr;
+    vector <int> sub = {};
+    for (int i =0;i<n;i++){
+        cin>>inp;
+        arr.push_back(inp);
+    }
+    
+    subsequence (0, arr, sub);
+   
+    return 0 ;
+}
