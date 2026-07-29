16ffa4316f60a9f26523286a0a82b16efc152d8e
rajparmar <rajparmarjb43@gmail.com>
2026-07-29 19:52:29 +0530

    dsa

--- subsequence.cpp
+++ subsequence.cpp
@@ -41,39 +41,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- void subsequence( int i , vector <int> &a , vector <int> &b){
+//  void subsequence( int i , vector <int> &a , vector <int> &b){
      
-    if (i>=a.size()){
-        cout << "{";
-        for (auto x : b){
-            cout << x ;
+//     if (i>=a.size()){
+//         cout << "{";
+//         for (auto x : b){
+//             cout << x ;
             
-        } 
-        cout << "}";
-    cout << endl;
-    return ;
-  }
+//         } 
+//         cout << "}";
+//     cout << endl;
+//     return ;
+//   }
      
-    b.push_back(a[i]) ;
-    subsequence(i+1,a,b);
+//     b.push_back(a[i]) ;
+//     subsequence(i+1,a,b);
     
-    b.pop_back();
-    subsequence(i+1,a,b);
+//     b.pop_back();
+//     subsequence(i+1,a,b);
     
+//      return ;
+//  }
+
+// int main (){
+//     int n ,inp ;
+//     cin >> n ;
+//     vector <int> arr;
+//     vector <int> sub = {};
+//     for (int i =0;i<n;i++){
+//         cin>>inp;
+//         arr.push_back(inp);
+//     }
+    
+//     subsequence (0, arr, sub);
+   
+//     return 0 ;
+// }
+
+
+void sub(int i , vector <int> &a , vector <int> &b ){
+     if(i>=a.size())
+     { 
+        cout << "{";
+        for(auto x : b){
+            cout << x << " "; 
+        }
+        cout << "}" << endl;
+        return ;
+     }
+     sub(i+1 , a , b);
+
+     b.push_back(a[i]);
+     sub(i+1, a , b);
+
+     b.pop_back();
+
      return ;
- }
+ 
+}
 
 int main (){
     int n ,inp ;
-    cin >> n ;
-    vector <int> arr;
-    vector <int> sub = {};
-    for (int i =0;i<n;i++){
-        cin>>inp;
-        arr.push_back(inp);
+    cin  >> n ;
+    vector <int> a,b;
+    for (int i =0 ; i< n ; i++){
+        cin >> inp;
+        a.push_back(inp);
     }
-    
-    subsequence (0, arr, sub);
-   
-    return 0 ;
+ 
+    sub(0,a,b);
+
+    return 0;
 }
