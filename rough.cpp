d60bdec1eb16679f72a4dbd984aaacc276828d82
rajparmar <rajparmarjb43@gmail.com>
2026-08-05 20:00:05 +0530

    trying

--- rough.cpp
+++ rough.cpp
@@ -2,9 +2,20 @@
 using namespace std;
 
 int main (){
-    int a=2;
-    int b=3;
-   swap(a,b);
-    cout << a<< b ;
+    vector<int > a ={1,2,3};
+    deque <int> b;
+    for(int i=0;i<=a.size();i++){
+        b.push_front(a.back());
+        a.pop_back();
+    }
+    for(auto x :b){
+        cout << x;
+    }
+
+
+//     int a=2;
+//     int b=3;
+//    swap(a,b);
+//     cout << a<< b ;
     return 0;
 }
