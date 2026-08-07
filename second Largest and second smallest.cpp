04a7d72406aa16166ebd1cef8b193daa4cda1328
rajparmar <rajparmarjb43@gmail.com>
2026-08-07 17:48:48 +0530

    more

--- second Largest and second smallest.cpp
+++ second Largest and second smallest.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- int largest(int arr[]){
+ int largest(int arr[],int n ){
     //1st approach
 //     sort(arr,arr+sizeof(arr));
 //    return arr[sizeof(arr)-1];
@@ -13,6 +13,10 @@ using namespace std;
    }
    return larg;
 }
+
+
+
+
 int slargest(int arr[],int n){
     //1st approach 
     //sort and search from last 
@@ -47,15 +51,97 @@ int slargest(int arr[],int n){
     }
     return slarge;
  }
- int smallest(int arr[]){
-     
-     int small=arr[0];
-     for(int i =1;i<sizeof(arr);i++){
-        if(small>arr[i]) small=arr[i];
-     }
-     return small;
+
+
+
+
+
+ int smallest(int arr[],int n){
+    //1st approach
+    sort(arr,arr+n);
+    return arr[0]; 
+
+
+    //  2mt approach
+    //  int small=arr[0];
+    //  for(int i =1;i<n;i++){
+    //     if(small>arr[i]) small=arr[i];
+    //  }
+    //  return small;
+
+
+
+
  }
- int ssmallest(int arr[]){}
+
+
+
+ int ssmallest(int arr[],int n){
+    //1st approach 
+    // int ssmall=INT_MIN;
+    //  sort(arr,arr+n);
+    //  for(int i =1;i<n;i++){
+    //     if(arr[i]>arr[0]){
+    //         ssmall=arr[i];
+    //         break;
+    //     }
+
+    //  }
+  //tm: O(nlogn+n)
+
+  //2nd approach 
+//    int small=arr[0];
+//    int ssmall=INT_MAX;
+   
+//    for(int i =1;i<n;i++){
+//     if(arr[i]<small) small=arr[i];
+//    }
+
+//    for(int i=0;i<n;i++){
+//     if(ssmall>arr[i]&&arr[i]>small){
+//         ssmall=arr[i];
+//     }
+// }
+// return ssmall;
+
+// tm:(2n)
+
+//3rd approach
+
+int small=arr[0];
+int ssmall =INT_MAX;
+
+for(int i=1;i<n;i++){
+    if(small>arr[i]){
+        ssmall=small;
+        small=arr[i];
+    }else if (arr[i]>small&&arr[i]<ssmall){
+        ssmall=arr[i];
+    }
+    return ssmall;
+    
+}
+
+
+ }
+
+
+
+     bool checkSorted( int arr[],int n){
+            
+
+     }
+
+
+
+
+
+
+
+
+
+
+
 int main(){
     int n;
     cin >> n;
@@ -76,6 +162,7 @@ int main(){
     // cout << slargest(arr,n);
     // cout << smallest(arr,n);
     // cout << ssmallest(arr,n);
+    cout << checkSorted(arr,n);
 
 
 }
